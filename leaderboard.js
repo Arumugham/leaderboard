f138990b835e4fd6ef2eaf8fdea2f519c5574080
Arumugham Sankaran <arumugham@localhost.localdomain>
2015-03-07 16:44:36 +0530

    git button increment but not cchanging

--- leaderboard.js
+++ leaderboard.js
@@ -1,1 +1,45 @@
 console.log("hello boss!!");
+PlayersList = new Mongo.Collection('players');
+// PlayersList.insert({ name: "Sachin", score: 20 });
+// PlayersList.insert({ name: "Laxman", score: 15 });
+// PlayersList.insert({ name: "Rahul", score: 10 });
+// PlayersList.insert({ name: "Kohli", score: 5 });
+// PlayersList.insert({ name: "Dhoni", score: 25 });
+// PlayersList.insert({ name: "Rohit", score: 0 });
+
+if(Meteor.isClient){
+	console.log("Hello Client");
+	Template.leaderboard.helpers({
+ 		'player':function(){
+ 			return PlayersList.find()
+ 		},
+ 		'selectedClass': function(){
+ 			var playerID = this._id;
+ 			var selectedPlayer = Session.get('selectedPlayer');
+ 			if(playerID == selectedPlayer){
+ 				return "selected"
+ 			}
+ 		},
+ 		'count': function(){
+ 			return PlayersList.find().count()
+ 		}
+ 	});
+ 	Template.leaderboard.events({
+ 		'click .player': function(){
+ 			var playerID = this._id;
+ 			Session.set('selectedPlayer', playerID);
+ 			var selectedPlayer = Session.get("selectedPlayer");
+ 			console.log(selectedPlayer);
+ 		},
+ 		'click .increment': function(){
+ 			var selectedPlayer = Session.get('selectedPlayer');
+ 			PlayersList.update(selectedPlayer, {$set: {score : 5}});
+ 		}
+ 	});
+}
+if(Meteor.isServer){
+	console.log("Server here maga!!");
+	//PlayersList.remove({});
+}
+
+
